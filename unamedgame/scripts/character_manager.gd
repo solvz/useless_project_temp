1140143080b8edfee03ef2ce93ad459cc5900717
solvz <krishnarajeev2005@gmail.com>
2025-08-09 05:25:24 +0530

    Updated Background, and added character selection

---
+++ unamedgame/scripts/character_manager.gd
@@ -0,0 +1,81 @@
+extends Node
+
+# Character types available
+enum CharacterType {
+	BLACK_CAT,
+	BROWN_CAT,
+	SIAMESE_CAT
+}
+
+# Currently selected character
+var selected_character: CharacterType = CharacterType.BLACK_CAT
+
+# Character data with their sprite paths
+var character_data = {
+	CharacterType.BLACK_CAT: {
+		"name": "Black Cat",
+		"idle_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BlackCat/PNG/IdleCatb.png",
+		"run_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BlackCat/PNG/RunCatb.png",
+		"jump_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BlackCat/PNG/JumpCabt.png",
+		"attack_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BlackCat/PNG/AttackCatb.png"
+	},
+	CharacterType.BROWN_CAT: {
+		"name": "Brown Cat",
+		"idle_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BrownCat/IdleCattt.png",
+		"run_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BrownCat/RunCattt.png",
+		"jump_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BrownCat/JumpCatttt.png",
+		"attack_sprite": "res://assets/sprites/Player/AnimalPack/Cats/BrownCat/AttackCattt.png"
+	},
+	CharacterType.SIAMESE_CAT: {
+		"name": "Siamese Cat",
+		"idle_sprite": "res://assets/sprites/Player/AnimalPack/Cats/Siamese/PNG/IdleCattt.png",
+		"run_sprite": "res://assets/sprites/Player/AnimalPack/Cats/Siamese/PNG/RunCattt.png",
+		"jump_sprite": "res://assets/sprites/Player/AnimalPack/Cats/Siamese/PNG/JumpCatttt.png",
+		"attack_sprite": "res://assets/sprites/Player/AnimalPack/Cats/Siamese/PNG/AttackCattt.png"
+	}
+}
+
+func get_character_name(character_type: CharacterType) -> String:
+	return character_data[character_type]["name"]
+
+func get_idle_sprite_path(character_type: CharacterType = selected_character) -> String:
+	return character_data[character_type]["idle_sprite"]
+
+func get_run_sprite_path(character_type: CharacterType = selected_character) -> String:
+	return character_data[character_type]["run_sprite"]
+
+func get_jump_sprite_path(character_type: CharacterType = selected_character) -> String:
+	return character_data[character_type]["jump_sprite"]
+
+func get_attack_sprite_path(character_type: CharacterType = selected_character) -> String:
+	return character_data[character_type]["attack_sprite"]
+
+func set_selected_character(character_type: CharacterType):
+	selected_character = character_type
+
+func get_all_character_types() -> Array:
+	return character_data.keys()
+
+func get_next_character_type() -> CharacterType:
+	var character_types = get_all_character_types()
+	var current_index = character_types.find(selected_character)
+	var next_index = (current_index + 1) % character_types.size()
+	return character_types[next_index]
+
+func get_previous_character_type() -> CharacterType:
+	var character_types = get_all_character_types()
+	var current_index = character_types.find(selected_character)
+	var prev_index = (current_index - 1) % character_types.size()
+	if prev_index < 0:
+		prev_index = character_types.size() - 1
+	return character_types[prev_index]
+
+func switch_to_next_character():
+	var next_character = get_next_character_type()
+	set_selected_character(next_character)
+	return next_character
+
+func switch_to_previous_character():
+	var prev_character = get_previous_character_type()
+	set_selected_character(prev_character)
+	return prev_character
--- unamedgame/scripts/idlecatmovement.gd
+++ unamedgame/scripts/idlecatmovement.gd
@@ -3,9 +3,69 @@ extends CharacterBody2D
 const SPEED = 200
 var fish_counter=0
 @onready var fish_label = %Label
+@onready var animated_sprite = $AnimatedSprite2D
+@onready var character_label = %CharacterInfo/CharacterLabel
 
+func _ready():
+	setup_character_sprites()
+	update_character_label()
+
+func setup_character_sprites():
+	# Get the current character's idle sprite
+	var idle_sprite_path = CharacterManager.get_idle_sprite_path()
+	var idle_texture = null
+	
+	if ResourceLoader.exists(idle_sprite_path):
+		idle_texture = load(idle_sprite_path)
+	else:
+		print("Warning: Could not find sprite at path: ", idle_sprite_path)
+		return
+	
+	# Clear existing animations
+	var sprite_frames = SpriteFrames.new()
+	
+	# Create idle animation
+	var idle_frames = []
+	
+	# Check if the texture is a sprite sheet (has multiple frames)
+	if idle_texture:
+		var texture_width = idle_texture.get_width()
+		var texture_height = idle_texture.get_height()
+		var frame_width = 32  # Assuming 32x32 frames
+		var frame_height = 32
+		var frames_per_row = texture_width / frame_width
+		var total_frames = min(7, frames_per_row)  # Max 7 frames for idle animation
+		
+		for i in range(total_frames):
+			var atlas_texture = AtlasTexture.new()
+			atlas_texture.atlas = idle_texture
+			atlas_texture.region = Rect2(i * frame_width, 0, frame_width, frame_height)
+			idle_frames.append(atlas_texture)
+	
+	# Add idle animation to sprite frames
+	sprite_frames.add_animation("idle")
+	for frame in idle_frames:
+		sprite_frames.add_frame("idle", frame)
+	sprite_frames.set_animation_speed("idle", 10.0)
+	sprite_frames.set_animation_loop("idle", true)
+	
+	# Apply the new sprite frames
+	animated_sprite.sprite_frames = sprite_frames
+	animated_sprite.play("idle")
 
 func _physics_process(delta: float) -> void:
+	# Handle character switching
+	if Input.is_action_just_pressed("switch_character_next"):
+		CharacterManager.switch_to_next_character()
+		setup_character_sprites()
+		update_character_label()
+		print("Switched to: ", CharacterManager.get_character_name(CharacterManager.selected_character))
+	elif Input.is_action_just_pressed("switch_character_prev"):
+		CharacterManager.switch_to_previous_character()
+		setup_character_sprites()
+		update_character_label()
+		print("Switched to: ", CharacterManager.get_character_name(CharacterManager.selected_character))
+	
 	var input_direction = Vector2(
 		Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left"),
 		Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
@@ -22,3 +82,7 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 func set_fish(new_fish_count: int) -> void:
 	fish_counter = new_fish_count
 	fish_label.text = "Fish Count: " + str(fish_counter)
+
+func update_character_label():
+	if character_label:
+		character_label.text = "Character: " + CharacterManager.get_character_name(CharacterManager.selected_character)
